7036c6ef835174afd2b4f5af29f03fafa17358bf
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-09-19 13:21:57 +0100

    test

---
+++ app/utils/create_user.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo useradd -m $1 -g ftpaccess -s /usr/sbin/nologin
--- app/utils/user_utils.py
+++ app/utils/user_utils.py
@@ -2,4 +2,4 @@ import os
 import subprocess
 
 def create_user(username):
-	proc = subprocess.Popen(['sudo','useradd', '-m', username, '-g', 'ftpaccess', '-s', '/usr/sbin/nologin'])
+	subprocess.call('create_user.sh')
