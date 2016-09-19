4bfa7aed8d0f93dc70273803cfce4b70650fd1df
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-09-19 13:50:17 +0100

    test

--- app/utils/user_utils.py
+++ app/utils/user_utils.py
@@ -5,7 +5,8 @@ import config
 def create_user(username):
 	os.putenv('INNUENDO_PASS', config.ADMIN_PASS)
 	print config.ADMIN_PASS
-	proc = subprocess.Popen([os.path.join(os.getcwd(),'app/utils/create_user.sh'), username], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	print os.path.join(os.getcwd(),'app/utils/create_user.sh')
+	proc = subprocess.Popen(['sh', os.path.join(os.getcwd(),'app/utils/create_user.sh'), username], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	print config.ADMIN_PASS
 	if proc.returncode != 0:
 		print 'STDOUT'
