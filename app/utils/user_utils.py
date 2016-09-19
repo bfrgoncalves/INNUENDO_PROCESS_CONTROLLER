6afbeb9c4801ac719b45c247c7359b2d3ec560b5
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-09-19 13:39:37 +0100

    test

--- app/utils/user_utils.py
+++ app/utils/user_utils.py
@@ -4,8 +4,9 @@ from config import ADMIN_PASS
 
 def create_user(username):
 	os.putenv('INNUENDO_PASS', ADMIN_PASS)
+	print ADMIN_PASS
 	proc = subprocess.Popen(['create_user.sh', username], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	
+	print ADMIN_PASS
 	if proc.returncode != 0:
 		print 'STDOUT'
 		print stdout.decode("utf-8")
