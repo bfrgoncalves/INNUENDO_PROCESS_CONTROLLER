4cb0c839d23bc3e1926ac80e73af7de61cd75ea7
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-09-19 15:54:18 +0100

    test

--- app/utils/user_utils.py
+++ app/utils/user_utils.py
@@ -5,8 +5,9 @@ import random
 import string
 
 def create_user(username, upload_folder):
+	new_username = username.split('@')[0]
 	os.putenv('INNUENDO_PASS', config.ADMIN_PASS)
-	proc = subprocess.Popen(['sh', os.path.join(os.getcwd(),'app/utils/create_user.sh'), username, upload_folder], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	proc = subprocess.Popen(['sh', os.path.join(os.getcwd(),'app/utils/create_user.sh'), new_username, upload_folder], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	stdout, stderr = proc.communicate()
 
 def random_letters(n):
