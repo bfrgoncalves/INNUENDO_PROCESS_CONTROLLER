d64bf54bd60fd025e65377de7e300c05f4dd2fb0
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-09-20 12:16:46 +0100

    test

--- app/utils/user_utils.py
+++ app/utils/user_utils.py
@@ -13,6 +13,12 @@ def create_user(username, upload_folder):
 	proc = subprocess.Popen(['sh', os.path.join(os.getcwd(),'app/utils/create_user.sh'), username, upload_folder, passw], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	stdout, stderr = proc.communicate()
 
+	if proc.returncode != 0:
+		print 'STDOUT'
+		print stdout.decode("utf-8")
+		print 'STDERR'
+		print stderr.decode("utf-8")
+
 	return passw
 
 def changepass(username):
