4914c7670c9b15c1a0ce25f57ed4a720b7f447fd
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-09-20 12:12:32 +0100

    test

--- app/utils/create_user.sh
+++ app/utils/create_user.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo $INNUENDO_PASS | sudo -S useradd -m $1 -g ftpaccess -s /usr/sbin/nologin
 #echo $INNUENDO_PASS | sudo -S passwd $1
-echo -e "$3\n$3\n$INNUENDO_PASS" | sudo -S passwd $1
+newpass=$3
+echo -e "$newpass\n$newpass\n$INNUENDO_PASS" | sudo -S passwd $1
 echo $INNUENDO_PASS | sudo -S chown root /home/$1
 echo $INNUENDO_PASS | sudo -S mkdir /home/$1/$2
 echo $INNUENDO_PASS | sudo -S chown $1:ftpaccess /home/$1/$2
