46230fe9c1d585d572d9a30abbfe91dad2ef6fe6
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-09-20 12:47:20 +0100

    test

--- app/utils/changepass.sh
+++ app/utils/changepass.sh
@@ -5,4 +5,6 @@ echo 'in1 ' $1
 echo 'in2 ' $2
 echo "$newpass\n$newpass\n$INNUENDO_PASS"
 echo -e "$newpass\n$newpass\n$INNUENDO_PASS"
+echo $(echo -e "$newpass\n$newpass\n$INNUENDO_PASS")
+echo $(echo "$newpass\n$newpass\n$INNUENDO_PASS")
 echo -e "$newpass\n$newpass\n$INNUENDO_PASS" | sudo -S passwd $1
