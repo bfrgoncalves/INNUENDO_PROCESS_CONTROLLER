60ce1d24c49c55af484d5230e465d40c15f0ff09
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-12-22 14:38:23 +0000

    launch_jobs

--- job_processing/queue_processor.py
+++ job_processing/queue_processor.py
@@ -41,7 +41,7 @@ class Queue_Processor:
 		#print key_value_args
 		key_value_args = [config['INNUCA_PATH']] + key_value_args
 		print key_value_args
-		os.system('./launch_job.sh "' + ' '.join(key_value_args) + '"')
+		os.system('sh job_processing/launch_job.sh "' + ' '.join(key_value_args) + '"')
 		#proc = subprocess.Popen(key_value_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		#if proc.returncode == 0:
 			#return True
