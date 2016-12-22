a36ecd1a9f854ecaec84a7373cde1fd3619171ad
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-12-22 15:18:20 +0000

    launch_jobs

--- job_processing/launch_job.sh
+++ job_processing/launch_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat job_processing/batch_innuca.template | sed -e 's/JOB_PARAMETERS/'$1'/' > sbatch_innuca.sh
+cat job_processing/sbatch_innuca.template | sed -e 's/JOB_PARAMETERS/'$1'/' > sbatch_innuca.sh
 
 sbatch sbatch_innuca.sh
