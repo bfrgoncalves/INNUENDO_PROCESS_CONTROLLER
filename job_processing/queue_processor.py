21a2bb633b7ba42b2e5189d337c609734cf311b3
Bruno Goncalves <bgoncalves@Glaurung.local>
2016-12-23 11:20:39 +0000

    launch_jobs

--- job_processing/queue_processor.py
+++ job_processing/queue_processor.py
@@ -49,7 +49,7 @@ class Queue_Processor:
 		stdout, stderr = proc.communicate()
 		print stdout
 		jobID = stdout.split(' ')
-		jobID = jobID[-1]
+		jobID = jobID[-1].strip('\n')
 		print jobID
 		if proc.returncode == 0:
 			return True
