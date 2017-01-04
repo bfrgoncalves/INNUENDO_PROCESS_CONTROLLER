ba79b3ededcdae1b3de92dcd47be8582ff040253
Bruno Goncalves <bgoncalves@Glaurung.local>
2017-01-04 16:37:53 +0000

    multiple tasks per job

--- job_processing/launch_job.sh
+++ job_processing/launch_job.sh
@@ -6,6 +6,6 @@ echo $2
 OLD=ARRAY_STRING
 TASKNUMBER=NUMBEROFTASKS
 cat job_processing/sbatch_innuca.template | sed "s#$OLD#$1#1" > sbatch_innuca.sh
-cat sbatch_innuca.sh | sed "s#$TASKNUMBER#$2#1" > sbatch_innuca.sh
+cat sbatch_innuca.sh | sed "s#$TASKNUMBER#$2#1" > sbatch_innuca_final.sh
 
-sbatch sbatch_innuca.sh
+sbatch sbatch_innuca_final.sh
