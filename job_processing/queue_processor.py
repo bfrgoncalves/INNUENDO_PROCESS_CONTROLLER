37991abea0e5b38b9f7bae6908d92f77aa021687
Bruno Goncalves <bgoncalves@Glaurung.local>
2017-01-04 11:57:02 +0000

    multiple tasks per job

--- job_processing/queue_processor.py
+++ job_processing/queue_processor.py
@@ -46,8 +46,8 @@ class Queue_Processor:
 
 			count_workflows += 1;
 			print workflow
-			parameters = json.loads(workflow['parameters'])['used Parameter']
-			parameters = parameters['used Parameter']
+			parameters = json.loads(workflow['parameters'])
+			#parameters = parameters['used Parameter']
 			print parameters
 			username = workflow['username']
 
