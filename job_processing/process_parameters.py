b88cc757d3a68a47e44cb485d21df12c1098a2b4
Bruno Goncalves <bgoncalves@Glaurung.local>
2017-01-06 16:02:30 +0000

    multiple tasks per job

--- job_processing/process_parameters.py
+++ job_processing/process_parameters.py
@@ -29,7 +29,8 @@ def process_innuca(key_value_args, parameters, user_folder):
 	key_value_args.append('-o')
 	key_value_args.append(os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID'))
 
-	after_application_steps = ';ln -s $(cat ' + os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID') + '/*/final_assembly.txt) ' + os.path.join(str(user_folder),"SLURM_ARRAY_JOB_ID") + '/innuca_assembly_SLURM_ARRAY_JOB_ID.fasta'
+	after_application_steps = 'mkdir ' + os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID') + '/INNUca; '
+	after_application_steps += '; ln -s $(cat ' + os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID') + '/*/final_assembly.txt) ' + os.path.join(str(user_folder),"SLURM_ARRAY_JOB_ID") + '/innuca_assembly_SLURM_ARRAY_JOB_ID.fasta;' 
 
 	#after_application_steps = 'count_assemblies=0; for file_found in $(find . -name final_assembly.txt); do (( count_assemblies++ )); ln -s $(cat $file_found) '+os.path.join(str(user_folder),"SLURM_ARRAY_JOB_ID")+'/innuca_assembly_SLURM_ARRAY_JOB_ID.fasta; done'
 
@@ -46,14 +47,14 @@ def process_chewbbaca(key_value_args, parameters, user_folder):
 	config = {}
 	execfile("config.py", config)
 
-	prev_application_steps = 'find ' + user_folder + '/SLURM_ARRAY_JOB_ID/*.fasta > ' + user_folder + '/SLURM_ARRAY_JOB_ID/listGenomes.txt;'
+	prev_application_steps = 'find ' + user_folder + '/SLURM_ARRAY_JOB_ID/*.fasta > ' + user_folder + '/SLURM_ARRAY_JOB_ID/listGenomes.txt; '
 	prev_application_steps += 'find ' + user_folder + '/AgalSchema/*.fasta > ' + user_folder + '/SLURM_ARRAY_JOB_ID/listGenes.txt;'
 
 	key_value_args.append('-i')
 	key_value_args.append(os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID', 'listGenomes.txt'))
 
 	key_value_args.append('-o')
-	key_value_args.append(os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID'))
+	key_value_args.append(os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID/'))
 
 	key_value_args.append('-b')
 	key_value_args.append(config['BLAST_PATH'])
@@ -61,8 +62,7 @@ def process_chewbbaca(key_value_args, parameters, user_folder):
 	key_value_args.append('-g')
 	key_value_args.append(os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID','listGenes.txt'))
 
-	after_application_steps = ''
-
+	after_application_steps = 'mkdir ' + os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID') + '/chewBBACA; '
 
 	return key_value_args, prev_application_steps, after_application_steps
 	
@@ -79,7 +79,6 @@ def process_parameters(parameters, user_folder, workflow):
 	wf_params = json.loads(workflow['parameters'])
 	if wf_params['used Software'] in config['APPLICATIONS_ARRAY']:
 		software = wf_params['used Software']
-
 
 	options = {'INNUca':process_innuca, 'chewBBACA':process_chewbbaca}
 
