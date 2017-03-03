3cf64757741b56880838dbe4e882548f3dd32c25
Bruno Goncalves <bgoncalves@Glaurung.local>
2017-03-03 17:03:41 +0000

    test

--- app/resources/jobs/jobs.py
+++ app/resources/jobs/jobs.py
@@ -36,7 +36,6 @@ def load_results_from_file(job_id, username):
 	array_of_results = []
 
 	for i in onlyfiles:
-		print i
 		data = open(os.path.join(user_folder,i)).read()
 		json_data = json.loads(data)
 		array_of_results.append(json_data)
