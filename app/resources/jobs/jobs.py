5eac11411be4492c5d05db18223fc342b067016b
Bruno Goncalves <bgoncalves@Glaurung.local>
2017-03-03 16:28:51 +0000

    test

--- app/resources/jobs/jobs.py
+++ app/resources/jobs/jobs.py
@@ -7,6 +7,9 @@ from job_processing.queue_processor import Queue_Processor
 import datetime
 import json
 import subprocess
+import os
+from os import listdir
+from os.path import isfile, join
 
 from rq import Queue #Queue
 from redis import Redis
@@ -16,13 +19,25 @@ job_post_parser.add_argument('data', dest='data', type=str, required=True, help=
 
 job_get_parser = reqparse.RequestParser()
 job_get_parser.add_argument('job_id', dest='job_id', type=str, required=True, help="Job ID")
+job_get_parser.add_argument('username', dest='username', type=str, required=True, help="Username")
 #job_post_parser.add_argument('username', dest='username', type=str, required=True, help="Username")
 #job_post_parser.add_argument('files', dest='files', type=str, required=True, help="Files to use")
 #parameters -> workflow_id
 
 #get workflow, get protocols, get protocol parameters, run process
 
-#def add_job_data_to_db():
+def load_results_from_file(job_id, username):
+
+	user_folder = '/home/users/' + username + '/' + job_id.split('_')[0] + '/INNUca'
+	onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+	results = {}
+
+	for i in onlyfiles:
+		print i
+		data = open(i).read()
+		json_data = json.loads(data)
+		print json_data
 
 
 class Job_queue(Resource):
@@ -44,6 +59,9 @@ class Job_queue(Resource):
 		stdout, stderr = proc1.communicate()
 		print stdout, len(stdout.split('\t'))
 
+		results = ''
+		store_in_db = False
+
 		if len(stdout.split('\t')) == 1:
 			commands = 'sh job_processing/get_completed_jobs.sh ' + job_id.split('_')[0]
 			proc1 = subprocess.Popen(commands.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -51,7 +69,8 @@ class Job_queue(Resource):
 			parts = stdout.split('\t')
 			if len(parts) == 0:
 				stdout = job_id + '\tFAILED'
-			#else:
-				#sadd_job_data_to_db
+			else:
+				results = load_results_from_file(job_id, args.username)
+				store_in_db = True
 
-		return stdout
+		return {'stdout':stdout, 'store_in_db':store_in_db, 'results':results}
