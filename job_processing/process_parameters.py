324a2f8fa8e6dac999d60eb01c90e8e41bc46cc7
Bruno Goncalves <bgoncalves@Glaurung.local>
2017-03-24 10:47:13 +0000

    test

--- job_processing/process_parameters.py
+++ job_processing/process_parameters.py
@@ -59,7 +59,7 @@ def process_chewbbaca(key_value_args, parameters, user_folder):
 
 	prev_application_steps = 'find ' + user_folder + '/SLURM_ARRAY_JOB_ID/*/*.fasta > ' + user_folder + '/SLURM_ARRAY_JOB_ID/listGenomes.txt; '
 	prev_application_steps += 'find ' + 'dependencies/chewBBACA/campy_scheme_2017/genes/*.fasta > ' + user_folder + '/SLURM_ARRAY_JOB_ID/listGenes.txt;'
-	prev_application_steps = 'mkdir ' + os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID') + '/chewBBACA; '
+	prev_application_steps += 'mkdir ' + os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID') + '/chewBBACA; '
 
 	key_value_args.append('-i')
 	key_value_args.append(os.path.join(str(user_folder),'SLURM_ARRAY_JOB_ID', 'listGenomes.txt'))
