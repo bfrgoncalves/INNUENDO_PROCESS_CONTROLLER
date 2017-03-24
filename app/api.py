73eb401c82ba141980ca7dca9ba5f23348fd0671
Bruno Goncalves <bgoncalves@Glaurung.local>
2017-03-24 15:33:03 +0000

    test

--- app/api.py
+++ app/api.py
@@ -2,8 +2,12 @@ from app import app
 from flask.ext.restful import Api
 
 from resources.jobs.jobs import Job_queue
+from resources.files.files import FilesResource
 
 #Setup API
 api = Api(app)
 
 api.add_resource(Job_queue, '/jobs/')
+
+#get files from user
+api.add_resource(FilesResource, '/files/')
