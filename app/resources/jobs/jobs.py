b425aa856015ea1d71feeea3fc79050c6dc4da15
Bruno Goncalves <bgoncalves@Glaurung.local>
2017-03-28 13:43:15 +0100

    test

--- app/resources/jobs/jobs.py
+++ app/resources/jobs/jobs.py
@@ -37,9 +37,12 @@ execfile("config.py", config)
 
 def load_results_from_file(job_id, username):
 
-	user_folder = '/home/users/' + username + '/' + job_id.split('_')[0] + '/*_' + job_id
+	user_folder = '/home/users/' + username + '/' + job_id.split('_')[0] + '/*_' + job_id + '/*'
 
 	print user_folder
+	
+	if 'chewBBACA' in user_folder:
+		user_folder += '/*'
 
 	onlyfiles = [f for f in glob.glob(user_folder)]
 
