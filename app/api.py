54d10ce9b96fe312c2d5d8961e1207e95b4334f4
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-04-28 14:56:45 +0100

    test_download

--- app/api.py
+++ app/api.py
@@ -2,6 +2,7 @@ from app import app
 from flask.ext.restful import Api
 
 from resources.jobs.jobs import Job_queue, FilesResource, DownloadFilesResource
+from resources.downloads.downloads import DownloadResults
 
 #Setup API
 api = Api(app)
@@ -13,3 +14,6 @@ api.add_resource(FilesResource, '/jobs/fastqs/')
 
 #download files to user area
 api.add_resource(DownloadFilesResource, '/jobs/download/')
+
+#download files to user area
+api.add_resource(DownloadResults, '/jobs/results/download/')
---
+++ app/resources/downloads/downloads.py
@@ -0,0 +1,38 @@
+from app import app
+from flask.ext.restful import Api, Resource, reqparse, abort, fields, marshal_with #filters data according to some fields
+from flask import jsonify
+
+from job_processing.queue_processor import Queue_Processor
+
+import datetime
+import json
+import subprocess
+import os
+from os import listdir
+from os.path import isfile, join
+
+from rq import Queue #Queue
+from redis import Redis
+
+import glob
+
+file_get_parser = reqparse.RequestParser()
+file_get_parser.add_argument('file_path', dest='file_path', type=str, required=True, help="File_path")
+
+
+#READ CONFIG FILE
+config = {}
+execfile("config.py", config)
+
+class DownloadResults(Resource):
+
+	def get(self):
+		args = file_get_parser.parse_args()
+		try:
+	        return send_file(args.file_path, as_attachment=True)
+	    except Exception as e:
+	    	print e
+	        self.Error(400)
+
+
+
