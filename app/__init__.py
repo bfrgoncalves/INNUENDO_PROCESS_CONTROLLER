1ee0ae37982c7d0cfe52e735528b95d19a189e33
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-03 16:55:53 +0100

    map outputs to processes

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
 import os 
-import franz.openrdf
 
 from franz.openrdf.sail.allegrographserver import AllegroGraphServer
 from franz.openrdf.repository.repository import Repository
 from franz.miniclient import repository
 from config import basedir,AG_HOST,AG_PORT,AG_REPOSITORY,AG_USER,AG_PASSWORD
 
-#from queryParse2Json import parseAgraphStatementsRes,parseAgraphQueryRes
+from queryParse2Json import parseAgraphStatementsRes,parseAgraphQueryRes
 
 #from config import obo,localNSpace,protocolsTypes,processTypes,processMessages
 
@@ -16,24 +15,5 @@ from config import basedir,AG_HOST,AG_PORT,AG_REPOSITORY,AG_USER,AG_PASSWORD
 app = Flask(__name__)
 app.config.from_object('config') #Reads the config file located at ../
 
-#setup agraph
-#server= AllegroGraphServer(AG_HOST, AG_PORT, AG_USER, AG_PASSWORD)
-#catalog = server.openCatalog()             ## default rootCatalog
-#print "Available repositories in catalog '%s':  %s" % (catalog.getName(), catalog.listRepositories())    
-#myRepository = catalog.getRepository(AG_REPOSITORY, Repository.OPEN)
-#myRepository.initialize()
-#dbconAg = myRepository.getConnection()
-#dedicateddbconAg = myRepository.getConnection()
-#print "Repository %s is up!  It contains %i statements." % (
-#	myRepository.getDatabaseName(), dbconAg.size())
-
-print '####################################################'
-
-from franz.openrdf.vocabulary.rdf import RDF
-from franz.openrdf.vocabulary.xmlschema import XMLSchema
-from franz.openrdf.query.query import QueryLanguage
-from franz.openrdf.model import URI
-
-print "BABABAB"
 
 from app import api
--- job_processing/get_program_input.py
+++ job_processing/get_program_input.py
@@ -5,10 +5,11 @@ import argparse
 from queryParse2Json import parseAgraphStatementsRes,parseAgraphQueryRes
 
 #from config import obo,localNSpace,protocolsTypes,processTypes,processMessages
-from franz.openrdf.vocabulary.rdf import RDF
-from franz.openrdf.vocabulary.xmlschema import XMLSchema
-from franz.openrdf.query.query import QueryLanguage
-from franz.openrdf.model import URI
+import os 
+
+from franz.openrdf.sail.allegrographserver import AllegroGraphServer
+from franz.openrdf.repository.repository import Repository
+from franz.miniclient import repository
 
 #READ CONFIG FILE
 config = {}
@@ -19,6 +20,33 @@ localNSpace = config["localNSpace"]
 protocolsTypes = config["protocolsTypes"]
 processTypes = config["processTypes"]
 processMessages = config["processMessages"]
+basedir = config["basedir"]
+AG_HOST = config["AG_HOST"]
+AG_PORT = config["AG_PORT"]
+AG_REPOSITORY = config["AG_REPOSITORY"]
+AG_USER = config["AG_USER"]
+AG_PASSWORD = config["AG_PASSWORD"]
+
+from queryParse2Json import parseAgraphStatementsRes,parseAgraphQueryRes
+
+#setup agraph
+server= AllegroGraphServer(AG_HOST, AG_PORT, AG_USER, AG_PASSWORD)
+catalog = server.openCatalog()             ## default rootCatalog
+#print "Available repositories in catalog '%s':  %s" % (catalog.getName(), catalog.listRepositories())    
+myRepository = catalog.getRepository(AG_REPOSITORY, Repository.OPEN)
+myRepository.initialize()
+dbconAg = myRepository.getConnection()
+dedicateddbconAg = myRepository.getConnection()
+print "Repository %s is up!  It contains %i statements." % (
+	myRepository.getDatabaseName(), dbconAg.size())
+
+print '####################################################'
+
+from franz.openrdf.vocabulary.rdf import RDF
+from franz.openrdf.vocabulary.xmlschema import XMLSchema
+from franz.openrdf.query.query import QueryLanguage
+from franz.openrdf.model import URI
+
 
 def get_process_input(project_id, pipeline_id, process_id):
 
