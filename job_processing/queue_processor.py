4151758fa48e7990eb3cb11f9cea75fb9eb043fb
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-05 14:51:58 +0100

    downloads changes

--- job_processing/queue_processor.py
+++ job_processing/queue_processor.py
@@ -124,9 +124,8 @@ class Queue_Processor:
 			ena_file.write('\n'.join(download_parameters.accession_numbers.split(",")) + '\n')
 		
 		f = open(os.path.join(user_folder, output_id + '_download.txt'), "w")
-		commands = 'python dependencies/getSeqENA/getSeqENA.py -l ' + ena_file_txt + ' -o ' + user_folder + ' > ' + os.path.join(user_folder, output_id + '_download.txt')
-		os.system(commands)
-		#proc1 = subprocess.Popen(commands.split(' '), stdout=f)
+		commands = 'python dependencies/getSeqENA/getSeqENA.py -l ' + ena_file_txt + ' -o ' + user_folder
+		proc1 = subprocess.Popen(commands.split(' '), stdout=f)
 		#stdout, stderr = proc1.communicate()
 
 		'''numbers = download_parameters.accession_numbers.split(",")
