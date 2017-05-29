3ee21e7f5cf8d8669b7e043cf34fa7ee4f9cec80
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-29 21:30:29 +0100

    test status

--- job_processing/get_program_input.py
+++ job_processing/get_program_input.py
@@ -63,9 +63,6 @@ def get_process_input(project_id, pipeline_id, process_id):
 
 		result.close()
 
-		print jsonResult
-		print procStr
-
 		if "biosamples sample" in jsonResult[0]["label"]:
 			sys.stdout.write('FirstProcess')
 		#elif "false" in jsonResult[0]["statusStr"]:
