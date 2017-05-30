86bb4453da991ba5d501434ad2b5593845d34823
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-30 22:06:22 +0100

    test

--- job_processing/queue_processor.py
+++ job_processing/queue_processor.py
@@ -120,10 +120,10 @@ class Queue_Processor:
 			if key_value_args != False:
 				key_value_args = [language, softwarePath] + key_value_args
 				with open(workflow_filepath, 'a') as jobs_file:
-					jobs_file.write(prev_application_steps.replace("STEPID", str(count_workflows)).replace(";", "\n").replace("SLURM_ARRAY_JOB_ID", "$SLURM_JOB_ID"))
-					jobs_file.write(' '.join(key_value_args).replace("STEPID", str(count_workflows)).replace(";", "\n").replace("SLURM_ARRAY_JOB_ID", "$SLURM_JOB_ID"))
-					jobs_file.write(after_application_steps.replace("STEPID", str(count_workflows)).replace(";", "\n").replace("SLURM_ARRAY_JOB_ID", "$SLURM_JOB_ID"))
-					jobs_file.write(status_definition.replace("STEPID", str(count_workflows)).replace(";", "\n").replace("SLURM_ARRAY_JOB_ID", "$SLURM_JOB_ID"))
+					jobs_file.write(prev_application_steps.replace("STEPID", str(count_workflows)).replace("SLURM_ARRAY_JOB_ID", "$1"))
+					jobs_file.write(' '.join(key_value_args).replace("STEPID", str(count_workflows)).replace("SLURM_ARRAY_JOB_ID", "$1"))
+					jobs_file.write(after_application_steps.replace("STEPID", str(count_workflows)).replace("SLURM_ARRAY_JOB_ID", "$1"))
+					jobs_file.write(status_definition.replace("STEPID", str(count_workflows)).replace("SLURM_ARRAY_JOB_ID", "$1"))
 				workflow_filenames.append(workflow_filepath)
 				'''processes_ids.append(process_ids)
 				workflows_ids.append(workflow_id)
