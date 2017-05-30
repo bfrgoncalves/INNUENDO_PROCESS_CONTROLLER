ccd2dc6ad9ec284a98a635ff1b31f15f08a83e95
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-30 22:45:29 +0100

    test

--- job_processing/process_parameters.py
+++ job_processing/process_parameters.py
@@ -22,9 +22,9 @@ def get_protocol_parameters(parameters):
 
 def process_innuca(key_value_args, parameters, user_folder, workflow):
 
-	prev_application_steps = 'badstatus="404"; firstprocess="FirstProcess"'
+	prev_application_steps = 'badstatus="404"; firstprocess="FirstProcess";'
 
-	prev_application_steps += 'p_innuendo_input=$(python job_processing/get_program_input.py --project ' + workflow["project_id"] + ' --pipeline ' + workflow["pipeline_id"] + ' --process ' + workflow["process_id"] + ' -t input);'
+	prev_application_steps += ' p_innuendo_input=$(python job_processing/get_program_input.py --project ' + workflow["project_id"] + ' --pipeline ' + workflow["pipeline_id"] + ' --process ' + workflow["process_id"] + ' -t input);'
 	
 	prev_application_steps += ' echo $p_innuendo_input;'
 	
