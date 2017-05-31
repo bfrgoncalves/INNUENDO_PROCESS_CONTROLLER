124991ca585a12f2ac606b5675eb78c10f178417
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-31 19:47:51 +0100

    test

--- job_processing/process_parameters.py
+++ job_processing/process_parameters.py
@@ -113,7 +113,7 @@ def process_chewbbaca(key_value_args, parameters, user_folder, workflow):
 		prev_application_steps += "cp dependencies/chewBBACA/"+schema_to_use+"/ToDeleteLoci.txt "+ user_folder + "/SLURM_ARRAY_JOB_ID/ToDeleteLoci.txt; ";
 
 		prev_application_steps += 'less dependencies/chewBBACA/' + '/'+schema_to_use+'/listGenes1.txt | rev | cut -d/ -f1 | rev > '+ user_folder + '/SLURM_ARRAY_JOB_ID/tocompare.txt; ';
-		prev_application_steps += "awk 'NR==FNR{a[$0]=1;next}!a[$0]' "+ user_folder + "/SLURM_ARRAY_JOB_ID/ToDeleteLoci.txt "+ user_folder + "/SLURM_ARRAY_JOB_ID/ToDeleteLoci.txt > "+ user_folder + "/SLURM_ARRAY_JOB_ID/listGenes.txt; ";
+		prev_application_steps += "awk 'NR==FNR{a[$0]=1;next}!a[$0]' "+ user_folder + "/SLURM_ARRAY_JOB_ID/ToDeleteLoci.txt "+ user_folder + "/SLURM_ARRAY_JOB_ID/tocompare.txt > "+ user_folder + "/SLURM_ARRAY_JOB_ID/listGenes.txt; ";
  
 		#prev_application_steps += "awk 'NR==FNR{a[$0]=1;next}!a[$0]' "+ user_folder + "/SLURM_ARRAY_JOB_ID/ToDeleteLoci.txt "+ 'dependencies/chewBBACA/' + '/'+schema_to_use+"/listGenes1.txt > "+ user_folder + "/SLURM_ARRAY_JOB_ID/listGenes.txt; "; 
 
