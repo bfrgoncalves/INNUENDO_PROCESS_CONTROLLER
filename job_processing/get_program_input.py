ea1e674084d90bfbf3f5f6f86f5a2ac2403574ee
Bruno Goncalves <bfgoncalves@medicina.ulisboa.pt>
2017-05-31 09:13:20 +0100

    test

--- job_processing/get_program_input.py
+++ job_processing/get_program_input.py
@@ -101,7 +101,8 @@ def get_process_status(project_id, pipeline_id, process_id):
 
 		queryString = "SELECT (str(?typelabel) as ?label) (str(?file1) as ?file_1) (str(?file2) as ?file_2) (str(?file3) as ?file_3) (str(?status) as ?statusStr) WHERE{<"+procStr+"> obo:RO_0002233 ?in. ?in a ?type.?type rdfs:label ?typelabel. OPTIONAL { <"+procStr+"> obo:NGS_0000097 ?status. ?in obo:NGS_0000092 ?file1; obo:NGS_0000093 ?file2; obo:NGS_0000094 ?file3; }}"
 		#queryString = "SELECT ?file1 ?file2 ?file3 ?type   WHERE {<"+procStr+"> obo:RO_0002233 ?in. ?in obo:NGS_0000092 ?file1.?in obo:NGS_0000093 ?file2.?in obo:NGS_0000094 ?file3. ?in a ?type}"
-		#print queryString
+		print queryString
+
 		tupleQuery = dbconAg.prepareTupleQuery(QueryLanguage.SPARQL, queryString)
 		result = tupleQuery.evaluate()
 		
