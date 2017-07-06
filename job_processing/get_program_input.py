ae84ad228648e056b49802b1e6d3d1a4c8e5de63
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-07 00:19:17 +0100

    404 to stdout

--- job_processing/get_program_input.py
+++ job_processing/get_program_input.py
@@ -74,7 +74,7 @@ def get_process_input(project_id, pipeline_id, process_id):
 			sys.stdout.write(jsonResult[0]["file_3"].split('"')[1])
 		#print jsonResult["file3"]
 	except Exception as e:
-		sys.stderr.write("404")
+		sys.stdout.write("404")
 
 		#change output to false
 		processURI = dbconAg.createURI(namespace=localNSpace+"projects/", localname=str(project_id)+"/pipelines/"+str(pipeline_id)+"/processes/"+str(process_id))
