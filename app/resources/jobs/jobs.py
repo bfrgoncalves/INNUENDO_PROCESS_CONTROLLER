bd838fdc3038b6874d22c891ec2a03dfa2d03510
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-07-07 08:10:29 +0100

    dont save report if results object is empty

--- app/resources/jobs/jobs.py
+++ app/resources/jobs/jobs.py
@@ -141,7 +141,8 @@ class Job_queue(Resource):
 			if "COMPLETED" in stdout or "WARNING" in stdout or "FAILED" in stdout:
 				print "COMPLETED"
 				results = load_results_from_file(job_id, args.username)
-				store_in_db = True
+				if len(results[1].keys()) store_in_db = False
+				else store_in_db = True
 
 			'''if len(parts) == 0:
 				stdout = job_id + '\tFAILED'
