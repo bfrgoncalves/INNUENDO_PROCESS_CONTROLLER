68995e8e0b3c2e79b5266ec9a470eb5097b7e12c
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-10-03 13:37:54 +0100

    submit chewbbaca job to other partition

--- job_processing/launch_job_chewbbaca.sh
+++ job_processing/launch_job_chewbbaca.sh
@@ -4,6 +4,7 @@ echo $1
 echo $2
 echo $3
 echo $4
+echo $5
 
 OLD=ARRAY_STRING
 #PROCESSES=ARRAY_PROCESSES
@@ -13,15 +14,17 @@ OLD=ARRAY_STRING
 #TOTALTASKSPERNODE=TASKSPERNODE
 USER_D=USER_DIR
 FILESTOTRANSFER=FILES_TO_TRANSFER
+DEPENDENCY=DEPENDENCY
 
 cat job_processing/sbatch_chewbbaca.template | sed "s#$OLD#$1#1" > sbatch_$4_1.sh
 #cat sbatch_innuca.sh | sed "s#$TASKNUMBER#$2#1" > sbatch_innuca_1.sh
 #cat sbatch_innuca_1.sh | sed "s#$TOTALTASKSPERNODE#$3#1" > sbatch_innuca_2.sh
 cat sbatch_$4_1.sh | sed "s#$FILESTOTRANSFER#$2#1" > sbatch_$4_2.sh
+cat sbatch_$4_2.sh | sed "s#$DEPENDENCY#$5#1" > sbatch_$4_3.sh
 #cat sbatch_innuca_3.sh | sed "s#$PROCESSES#$6#1" > sbatch_innuca_4.sh
 #cat sbatch_innuca_4.sh | sed "s#$WORKFLOWS#$7#1" > sbatch_innuca_5.sh
 #cat sbatch_innuca_5.sh | sed "s#$OUTPUTS#$8#1" > sbatch_innuca_6.sh
-cat sbatch_$4_2.sh | sed "s#$USER_D#$3#1" > sbatch_$4.sh
+cat sbatch_$4_3.sh | sed "s#$USER_D#$3#1" > sbatch_$4.sh
 
 rm sbatch_$4_*.sh
 
