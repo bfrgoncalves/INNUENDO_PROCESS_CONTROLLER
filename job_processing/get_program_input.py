e8828eed6e1efd622e0440ff04851b9ac3341c8f
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-10-14 12:07:28 +0100

    check input chewbbaca

--- job_processing/get_program_input.py
+++ job_processing/get_program_input.py
@@ -53,14 +53,12 @@ def get_process_input(project_id, pipeline_id, process_id):
 
 	try:
 		procStr = localNSpace + "projects/" + str(project_id) + "/pipelines/" + str(pipeline_id) + "/processes/" + str(process_id)
-		print procStr
 		
 		queryString = "SELECT (str(?typelabel) as ?label) (str(?file1) as ?file_1) (str(?file2) as ?file_2) (str(?file3) as ?file_3) (str(?status) as ?statusStr) WHERE{<"+procStr+"> obo:RO_0002233 ?in. ?in a ?type.?type rdfs:label ?typelabel. OPTIONAL { ?in obo:NGS_0000092 ?file1; obo:NGS_0000093 ?file2; obo:NGS_0000094 ?file3; } OPTIONAL { <"+procStr+"> obo:NGS_0000097 ?status.}}"
 		#queryString = "SELECT ?file1 ?file2 ?file3 ?type   WHERE {<"+procStr+"> obo:RO_0002233 ?in. ?in obo:NGS_0000092 ?file1.?in obo:NGS_0000093 ?file2.?in obo:NGS_0000094 ?file3. ?in a ?type}"
 		tupleQuery = dbconAg.prepareTupleQuery(QueryLanguage.SPARQL, queryString)
 		result = tupleQuery.evaluate()
 
-		print queryString
 		jsonResult=parseAgraphQueryRes(result,["file_3", "label", "statusStr"])
 		#jsonResult=parseAgraphQueryRes(result,["label"])
 
