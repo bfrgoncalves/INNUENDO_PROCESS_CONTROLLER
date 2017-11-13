3de300cab0d4cde860113d7e54482378a1a024a9
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-13 16:24:42 +0000

    add nextflow excecutor and ngsonto post routes

--- job_processing/bash_scripts/nextflow_executor.sh
+++ job_processing/bash_scripts/nextflow_executor.sh
@@ -10,9 +10,17 @@ echo $3
 echo $4
 #Post URL
 echo $5
-#Current dir
-echo$6
+#File 1
+echo $6
+#File 2
+echo $7
 
 cd $1
+
+if [ ! -d "data" ]; then
+	mkdir data
+fi
+
+ln -s $6 ${1}/data
+ln -s $7 ${1}/data
 nextflow run $2 --projectId=$3  --pipelineId=$4 --platformHTTP=$5 -profile slurm
-cd $6
--- job_processing/queue_processor.py
+++ job_processing/queue_processor.py
@@ -128,6 +128,11 @@ class Queue_Processor:
 
 			nextflow_tags.append(nextflow_tag+":"+processes_id)
 			task_ids.append(random_tag)
+
+			array_of_files = []
+
+			for x in files:
+				array_of_files.append(os.path.join(submitter_folder, config['FTP_FILES_FOLDER'],files[x]))
 			#processes_ids.append(processes_ids)
 
 
@@ -194,7 +199,7 @@ class Queue_Processor:
 
 
 		#RUN NEXTFLOW
-		commands = ['sh', 'job_processing/bash_scripts/nextflow_executor.sh', nexflow_user_dir, nextflow_file_location, project_id, pipeline_id, config["JOBS_ROOT_SET_OUTPUT"], cwd]
+		commands = ['sh', 'job_processing/bash_scripts/nextflow_executor.sh', nexflow_user_dir, nextflow_file_location, project_id, pipeline_id, config["JOBS_ROOT_SET_OUTPUT"], array_of_files[0], array_of_files[1]]
 		print commands
 		proc = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		stdout, stderr = proc.communicate()
