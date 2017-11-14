fa89eb26615ba97c445a5155362180d2c4aa4991
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-14 10:48:24 +0000

    arg parse for single parameter change

--- app/resources/jobs/jobs.py
+++ app/resources/jobs/jobs.py
@@ -256,7 +256,9 @@ class SetNGSOntoOutput(Resource):
 	def put(self):
 		parameters = request.get_json()
 		print parameters
+		print type(parameters)
 		parameters = json.loads('"'+parameters+'"')
+		print type(parameters)
 		print parameters
 		commands = ["python", "job_processing/get_program_input.py", "--project", parameters["project_id"], "--pipeline", parameters["pipeline_id"], "--process", parameters["process_id"], "-u", parameters["run_property"], "-v1", parameters["run_property_value"], "-t", parameters["type"]]
 		print commands
