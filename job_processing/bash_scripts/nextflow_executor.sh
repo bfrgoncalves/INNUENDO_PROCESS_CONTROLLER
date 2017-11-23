eefb28566f05d6b2c4b8511ae3794e9ba5bbaf47
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2017-11-23 15:43:35 +0000

    send new reports from nextflow

--- job_processing/bash_scripts/nextflow_executor.sh
+++ job_processing/bash_scripts/nextflow_executor.sh
@@ -18,6 +18,10 @@ echo $7
 echo $8
 #Reports HTTP route
 echo $9
+#user_name
+echo $10
+#user_id
+echo $11
 
 cd $1
 
@@ -27,4 +31,4 @@ fi
 
 ln -s $6 ${1}/data
 ln -s $7 ${1}/data
-nextflow run $2 --projectId=$3  --pipelineId=$4 --platformHTTP=$5 --sampleName=$6 --reportHTTP=$9 -profile oneida &
+nextflow run $2 --projectId=$3  --pipelineId=$4 --platformHTTP=$5 --sampleName=$6 --reportHTTP=$9 --currentUserName=$10 --currentUserId=$11 -profile oneida &
