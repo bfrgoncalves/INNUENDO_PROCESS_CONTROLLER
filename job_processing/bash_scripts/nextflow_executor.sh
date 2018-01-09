e2e73801a34d3745a812331b55e2b15205afe65b
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-09 16:34:26 +0000

    add nextflow profile parameter

--- job_processing/bash_scripts/nextflow_executor.sh
+++ job_processing/bash_scripts/nextflow_executor.sh
@@ -26,6 +26,8 @@ echo $11
 echo $12
 #genome size
 echo $13
+#nextflow profile
+echo $14
 
 cd $1
 
@@ -35,4 +37,4 @@ fi
 
 ln -s $6 ${1}/data
 ln -s $7 ${1}/data
-nextflow run $2 --projectId=$3  --pipelineId=$4 --platformHTTP=$5 --sampleName=$6 --reportHTTP=$9 --currentUserName=$10 --currentUserId=$11 --species=$12 -profile oneida  --genomeSize=$13 &
+nextflow run $2 --projectId=$3  --pipelineId=$4 --platformHTTP=$5 --sampleName=$6 --reportHTTP=$9 --currentUserName=$10 --currentUserId=$11 --species=$12 -profile $14 --genomeSize=$13 &
