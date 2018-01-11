7d07217ba9a908e4f5e6cd6c222bcd31f3031a7c
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-11 11:31:43 +0000

    add resume

--- job_processing/bash_scripts/nextflow_executor.sh
+++ job_processing/bash_scripts/nextflow_executor.sh
@@ -37,4 +37,4 @@ fi
 
 ln -s $7 ${1}/data/sample_1.fastq.gz
 ln -s $8 ${1}/data/sample_2.fastq.gz
-nextflow run $2 --projectId=$3  --pipelineId=$4 --platformHTTP=$5 --sampleName=$6 --reportHTTP=$9 --currentUserName=$10 --currentUserId=$11 --species=$12 -profile $14 --genomeSize=$13 -with-trace &
+nextflow run $2 --projectId=$3  --pipelineId=$4 --platformHTTP=$5 --sampleName=$6 --reportHTTP=$9 --currentUserName=$10 --currentUserId=$11 --species=$12 -profile $14 --genomeSize=$13 -resume -with-trace &
