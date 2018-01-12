9e9764d3af42d1d968dac6ffd2d228fcac94bf74
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-12 13:38:11 +0000

    add cache nextflow file and retur pid for subprocess

--- job_processing/queue_processor.py
+++ job_processing/queue_processor.py
@@ -155,7 +155,7 @@ class Queue_Processor:
 		nextflow_file_location = os.path.join(nexflow_user_dir, random_pip_name)
 		cwd = os.getcwd()
 		
-		commands = ['python3', config["NEXTFLOW_GENERATOR_PATH"]] + ["-t"] + nextflow_tags + ["-o", os.path.join(nexflow_user_dir, nextflow_file_location), "--include-templates", "--debug"]
+		commands = ['python3', config["NEXTFLOW_GENERATOR_PATH"]] + ["-t"] + nextflow_tags + ["-o", os.path.join(nexflow_user_dir, nextflow_file_location), "--include-templates"]
 		print commands
 		proc = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		stdout, stderr = proc.communicate()
