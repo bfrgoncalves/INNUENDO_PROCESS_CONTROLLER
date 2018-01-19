37acdd6600100c6f90c154757b1725b2dd90c488
bfrgoncalves <bfgoncalves@medicina.ulisboa.pt>
2018-01-19 19:41:38 +0000

    add files for pathotyping and serotyping

--- job_processing/bash_scripts/nextflow_executor.sh
+++ job_processing/bash_scripts/nextflow_executor.sh
@@ -54,6 +54,6 @@ ln -s $8 ${1}/data/sample_2.fastq.gz
 
 echo "\nincludeConfig 'platform.config'" >> nextflow.config
 
-nextflow run $2 --projectId=$3  --pipelineId=$4 --platformHTTP=$5 --sampleName=$6 --reportHTTP=$9 --currentUserName=$10 --currentUserId=$11 --species=$12 -profile $14 --genomeSize=$13 --schemaPath=$15 --schemaSelectedLoci=$16 --schemaCore=$17 --chewbbacaSpecies=$18 --referenceFileO=$19 --referenceFileH=$20 --pathoSpecies=$21 -resume > nextflow_log.txt 2>&1 &
+nextflow run $2 --projectId=$3  --pipelineId=$4 --platformHTTP=$5 --sampleName=$6 --reportHTTP=$9 --currentUserName=$10 --currentUserId=$11 --species=$12 -profile $14 --genomeSize=$13 --schemaPath=$15 --schemaSelectedLoci=$16 --schemaCore=$17 --chewbbacaSpecies=$18 --referenceFileO=$19 --referenceFileH=$20 --pathoSpecies="$21" -resume > nextflow_log.txt 2>&1 &
 
 echo $! > process.id
